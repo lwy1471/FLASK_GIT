f0b2bd30495c2f78bcf4b7e595246b172cef5c39
lwy1471 <lwy1471@gmail.com>
2018-03-23 17:53:08 +0900

    ing : Step 6: The View Functions

--- flask_server/exercise/flaskr/flaskr/flaskr.py
+++ flask_server/exercise/flaskr/flaskr/flaskr.py
@@ -19,3 +19,24 @@ def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    init_db()
+    print('Initialized the Database.')
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
