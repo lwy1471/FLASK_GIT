c7a924b0744ab339a311ecca6072ca71f258e155
lwy1471 <lwy1471@gmail.com>
2018-03-23 18:00:41 +0900

    ing : Step 6: The View Functions

--- flask_server/exercise/flaskr/flaskr/flaskr.py
+++ flask_server/exercise/flaskr/flaskr/flaskr.py
@@ -1,6 +1,6 @@
 import os
 import sqlite3
-from flask import Flask, request, sessions, g, redirect, url_for, abort,\
+from flask import Flask, request, session, g, redirect, url_for, abort,\
     render_template, flash
 
 app = Flask(__name__)
@@ -40,3 +40,21 @@ def get_db():
 def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
+
+@app.route('/')
+def show_entries():
+    db = get_db()
+    cur = db.excute('select title, text from entries oder by id secs')
+    entires = cur.fetchall()
+    return render_template('show_entries.html', entires=entires)
+
+@app.route('/add', method=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('insert into entries (title, text) values (?, ?)', [request.form['title'], request.form['text']])
+
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
