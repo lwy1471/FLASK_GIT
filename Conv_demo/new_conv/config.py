9d8505b0be63a042bcbe2f4ed17947fdf4bfd8ae
lwy1471 <lwy1471@gmail.com>
2018-03-26 16:59:08 +0900

    add conv demo

---
+++ Conv_demo/new_conv/config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+##### Conversation Service Credentials info #####
+class Config :
+    conversation_endpoint = "https://gateway.aibril-watson.kr/conversation/api"
+    conversation_username = "57b4553d-929e-4217-bb78-5fbcff381e04"
+    conversation_password = "JpBCh5hc6s2E"
+    workspace_main = "6f538c4c-cef5-45c5-ad58-417845712864"
